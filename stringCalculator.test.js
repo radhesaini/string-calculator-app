a8528cfbfa0872cc2827297eba614a47b86da0bf
iamradhe <52653889+radhesaini@users.noreply.github.com>
2025-01-13 11:47:37 +0530

    add more edge test cases

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -32,4 +32,27 @@ describe("StringCalculator", () => {
       "negative numbers not allowed -2,-4"
     );
   });
+  test("check given input is string or not", () => {
+    expect(() => calculator.add(["1,3"])).toThrow("Invalid user input!");
+  });
+
+  test("Ignore numbers greater than 1000", () => {
+    expect(calculator.add("1,2,1001")).toBe(3);
+  });
+
+  test("check for sum greater than 1000", () => {
+    expect(calculator.add("1000,1")).toBe(1001);
+  });
+  test("check for Consecutive delimiters", () => {
+    expect(calculator.add("1,,2")).toBe(3);
+  });
+  test("check for Trailing delimiters", () => {
+    expect(calculator.add("1,./")).toBe(1);
+  });
+  test("check for Leading delimiters", () => {
+    expect(calculator.add(",./1")).toBe(1);
+  });
+  test("check for Leading and Trailing delimiters", () => {
+    expect(calculator.add("//\\,./1\n\\")).toBe(1);
+  });
 });
